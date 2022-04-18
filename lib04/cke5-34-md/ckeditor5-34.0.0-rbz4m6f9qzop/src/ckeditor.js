5aa4a1ecb051b617301bdf97052fca8e8ee8dc39
Theo Armour <theo-armour@users.noreply.github.com>
2022-04-18 16:19:37 -0700

    update readme

---
+++ lib04/cke5-34-md/ckeditor5-34.0.0-rbz4m6f9qzop/src/ckeditor.js
@@ -0,0 +1,67 @@
+/**
+ * @license Copyright (c) 2014-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import FindAndReplace from '@ckeditor/ckeditor5-find-and-replace/src/findandreplace.js';
+import GeneralHtmlSupport from '@ckeditor/ckeditor5-html-support/src/generalhtmlsupport.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import Markdown from '@ckeditor/ckeditor5-markdown-gfm/src/markdown.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
+import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting.js';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
+
+class Editor extends ClassicEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Autoformat,
+	Bold,
+	Essentials,
+	FindAndReplace,
+	GeneralHtmlSupport,
+	Heading,
+	HorizontalLine,
+	Italic,
+	Link,
+	List,
+	Markdown,
+	Paragraph,
+	RemoveFormat,
+	SourceEditing,
+	Strikethrough
+];
+
+// Editor configuration.
+Editor.defaultConfig = {
+	toolbar: {
+		items: [
+			'heading',
+			'|',
+			'bold',
+			'italic',
+			'link',
+			'bulletedList',
+			'numberedList',
+			'horizontalLine',
+			'strikethrough',
+			'|',
+			'sourceEditing',
+			'findAndReplace',
+			'removeFormat',
+			'undo',
+			'redo'
+		]
+	},
+	language: 'en'
+};
+
+export default Editor;
